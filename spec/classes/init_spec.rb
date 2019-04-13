e45fd04b78c1893c6b954a8784a8596da4616bc3
Alex Harvey <alexharv074@gmail.com>
2019-04-13 19:43:04 +1000

    Add a failing test
    Adds the test to detect invalid JSON in the catalog.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper'
+require 'json'
 
 describe 'loopback' do
   it 'compiles' do
     is_expected.to compile
     File.write('catalogs/loopback.json', PSON.pretty_generate(catalogue))
+  end
+
+  it 'should contain file /server/config.json' do
+    is_expected.to contain_file('/server/config.json').with({
+      'ensure' => 'present',
+      'owner' => 'root',
+      'group' => 'root',
+      'mode' => '0444',
+    })
+  end
+
+  it '/server/config.json should be valid JSON' do
+    json_data = catalogue.resource('file', '/server/config.json').send(:parameters)[:content]
+    expect { JSON.parse(json_data) }.to_not raise_error
   end
 end
