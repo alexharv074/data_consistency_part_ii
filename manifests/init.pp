042e83b8a3ad4e5f673bc3fac8470f7318767580
Alex Harvey <alexharv074@gmail.com>
2019-04-13 19:46:32 +1000

    Refactor to use sourced file
    For code example 2. The ERB template was moved to files/config.json,
    with the missing comma typo restored and all the data hardcoded in it.
    Tests changed so that all tests are passing at this point.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,10 @@
 class loopback {
 
-  $rest_api_root = '/api'
-  $host = '0.0.0.0'
-  $port = 3000
-
   file { '/server/config.json':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0444',
-    content => template('loopback/config.json.erb'),
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0444',
+    source => 'puppet:///modules/loopback/config.json',
   }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -15,17 +15,4 @@ describe 'loopback' do
       'mode' => '0444',
     })
   end
-
-  it '/server/config.json should be valid JSON' do
-    json_data = catalogue.resource('file', '/server/config.json').send(:parameters)[:content]
-    expect { JSON.parse(json_data) }.to_not raise_error
-  end
-
-  it 'restApiRoot, host and port should look ok' do
-    json_data = catalogue.resource('file', '/server/config.json').send(:parameters)[:content]
-    parsed = JSON.parse(json_data)
-    expect(parsed['restApiRoot']).to match %r{^/[\w/]+$}
-    expect(parsed['host']).to match /^(\d+(\.|$)){4}$/
-    expect(parsed['port']).to be_a(Fixnum)
-  end
 end
